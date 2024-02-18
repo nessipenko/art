96be59c8d88f5fed9def19c00a12748bada7966a
nessipenko <101459146+nessipenko@users.noreply.github.com>
2024-02-19 00:29:56 +0100

    modules&slider

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', () => {
     'use strict'
 
     modals()
-    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn')
+    sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn')
     sliders('.main-slider-item', 'vertical')
 
 })
